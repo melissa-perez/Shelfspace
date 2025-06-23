09f6b5de768f10721c37d0b76f18bdfb61cca47d
Melissa Perez <perezmelissa053@gmail.com>
2025-06-23 00:20:45 -0400

    feat: fix not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router';
 
+import "../../index.css"
+import notFoundStyles from './NotFoundPage.module.css';
+
 function NotFoundPage() {
   return (
-    <>
+    <div className={`container ${notFoundStyles.notFoundContainer}`}>
       <p>Page not found.</p>
       <Link to={'/'}>Go back home</Link>
-    </>
+    </div>
   );
 }
 
